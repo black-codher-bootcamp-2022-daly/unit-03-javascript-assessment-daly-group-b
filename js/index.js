a99c8647f85f1258d8a3d00a2ece245eab75bcbf
C-E-L7 <carrera.eley@gmail.com>
2022-11-21 02:09:50 +0000

    changed grammar made 5 corrections commit three

--- js/index.js
+++ js/index.js
@@ -16,34 +16,43 @@ const project = document.querySelector(".timeline");
 const firstDivContent = `<div class = "container"><div class = "casing"><h2 class= "timeline-item-title"> Yeast </h2>
 <span class = "timeline-item-date">2600 BC</span>
 <p class = "timeline-item-summary">The Egyptians are the first recorded civilization to use yeast.</p>
-<button class = "timeline-item-more-info" id= "moreButton" data-open = "learnmore" >more details </button></div>
+<button class = "timeline-item-more-info" id= "moreButton" data-open = "learnmore" >More Details </button></div>
 <div class = "casing">
 <h2 class= "timeline-item-title"> Sugar </h2>
+
 <span class = "timeline-item-date">1069 AD</span>
 <p class = "timeline-item-summary">The first recorded use of sugar in England.</p>
-<button class = "timeline-item-more-info">more details </button></div>
+<button class = "timeline-item-more-info">More Details </button></div>
 </div>
 <div class = "casing">
-<h2 class= "timeline-item-title"> Icing sugar </h2>
+<h2 class= "timeline-item-title"> Icing Sugar </h2>
+
+
 <span class = "timeline-item-date">1494 AD</span>
 <p class = "timeline-item-summary">Icing sugar was invented.</p>
-<button class = "timeline-item-more-info">learn more </button></div>
+<button class = "timeline-item-more-info">More Details</button></div>
 </div>
 <div class = "casing"><h2 class= "timeline-item-title"> Soufflé </h2>
+
 <span class = "timeline-item-date">1700s AD</span>
 <p class = "timeline-item-summary">Soufflé appears in France</p>
-<button class = "timeline-item-more-info">learn more </button></div>
+<button class = "timeline-item-more-info">More Details </button></div>
 <div class = "casing"><h2 class= "timeline-item-title"> Milling </h2>
+
 <span class = "timeline-item-date">1779 AD</span>
-<p class = "timeline-item-summary">The first steam mill was erected in London.</p><button class = "timeline-item-more-info">more details </button></div>
+<p class = "timeline-item-summary">The first steam mill was erected in London.</p><button class = "timeline-item-more-info">More Details</button></div>
 <div class = "casing">
-<h2 class= "timeline-item-title"> Baking powder </h2>
+<h2 class= "timeline-item-title"> Baking Powder </h2>
+
 <span class = "timeline-item-date">1843 AD</span>
 <p class = "timeline-item-summary">The first modern version of baking powder was discovered and manufactured by British chemist Alfred Bird.</p>
-<button class = "timeline-item-more-info">more details </button></div>
-<div class = "casing"><h2 class= "timeline-item-title"> Synthetic organic dye </h2>
+<button class = "timeline-item-more-info">More Details</button></div>
+<div class = "casing"><h2 class= "timeline-item-title"> Synthetic Organic Dye </h2>
+
 <span class = "timeline-item-date">1856 </span>
-<p class = "timeline-item-summary">The first synthetic organic dye was discovered.</p></div>
+<p class = "timeline-item-summary">The first synthetic organic dye was discovered.</p>
+<button class = "timeline-item-more-info">More Details </button></div>
+</div>
 </div>`;
 
 //adding modals to page content
